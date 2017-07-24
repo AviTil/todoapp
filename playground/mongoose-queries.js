5d5f6df6d99ebdb5eb33f8dca7eb91c6aa836130
User <user@users-mbp.home>
2017-07-24 10:17:18 +0100

    add mongoose queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,53 @@
+var ObjectID = require('mongodb').ObjectID
+
+var mongoose = require('./../server/db/mongoose.js').mongoose
+var Todo = require('./../server/models/todo.js').Todo
+var User = require('./../server/models/user.js').User
+
+/*var id = '5970df834d16ba120ca48be8'
+
+if (!ObjectID.isValid(id)){
+    console.log('ID not valid')
+} 
+
+// finding all documents where a property equals a set value
+
+Todo.find({
+    _id: id
+}).then(function(docs){
+    console.log('Todos: '+docs)
+});
+
+Todo.findOne({
+    _id: id
+}).then(function(doc){
+    console.log('Todo: '+doc)
+});
+
+// finding all documents where ID matches a given value
+
+Todo.findById(id).then(function(doc){
+    
+    if (!doc){
+        return console.log('Todo by ID: Id not found')
+    }
+    
+    console.log('Todo by Id: '+doc)
+}).catch(function(e){
+    console.log(e)
+});*/
+
+
+var userID = '596dd4723168480a479a19c7'
+
+User.findById(userID).then(function(doc){
+    if (!doc) {
+        return console.log('User: no user found')
+    } else {
+        console.log('User: '+doc)
+    }
+}).catch(function(e){
+    console.log(e)
+});
+
+
